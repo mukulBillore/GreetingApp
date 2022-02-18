983aef36e69d4f332469213c8796ff396a6ca8dd
mukulBillore <mukulbillore88@gmail.com>
2022-02-18 09:23:00 +0530

    updated  :  used service layer to get simple greet message

--- src/main/java/com/bridgelabs/greetingAppDevelopment/controller/GreetingController.java
+++ src/main/java/com/bridgelabs/greetingAppDevelopment/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.bridgelabs.greetingAppDevelopment.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,25 +12,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabs.greetingAppDevelopment.model.Greeting;
-
+import com.bridgelabs.greetingAppDevelopment.service.GreetingService;
 
 @RestController
 public class GreetingController {
 	private static final String template = "Hello %s";
-    private final AtomicLong counter = new AtomicLong();
-    
+	private final AtomicLong counter = new AtomicLong();
+
+	@Autowired
+	GreetingService serve;
+
 	@GetMapping("/")
 	public String sayHello() {
 		return "hello from bridgelabs";
 	}
 
-	@GetMapping("/greeting/{name}")
+	@PostMapping("/greeting/{name}")
 	public Greeting sayHelloByPathVar(@PathVariable String name) {
-		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
-	@GetMapping("/body")
+	@PutMapping("/body")
 	public String sayHelloByBody(@RequestBody Greeting greeting) {
-		return "hello  "+greeting.getContent()+"id is  "+greeting.getId() ;
+		return "hello  " + greeting.getContent() + "id is  " + greeting.getId();
+	}
+	 
+	@GetMapping("/getServe")
+	public String sayHelloByService() {
+		return serve.getMessage();
 	}
 }
---
+++ src/main/java/com/bridgelabs/greetingAppDevelopment/service/GreetingService.java
@@ -0,0 +1,13 @@
+package com.bridgelabs.greetingAppDevelopment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+	public String getMessage() {
+		
+		return "hello world";
+	}
+	
+}
